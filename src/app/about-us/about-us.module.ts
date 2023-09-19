2ea4d1b0e5f5035e2049b5c52a244c9e53fc9000
Miriam2106 <20203tn055@utez.edu.mx>
2023-09-19 17:28:58 -0600

    initial commit

---
+++ src/app/about-us/about-us.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+import { AboutUsRoutingModule } from './about-us-routing.module'
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, AboutUsRoutingModule],
+})
+export class AboutUsModule {}
