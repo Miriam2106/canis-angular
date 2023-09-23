0566dd2f4eee6477ea2d193d59917bd9932da4aa
Ricardo C. <20203tn077@utez.edu.mx>
2023-09-23 16:24:03 -0600

    feat: add main view

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,14 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
-import { MainRoutingModule } from './main-routing.module';
-
+import { MainRoutingModule } from './main-routing.module'
+import { MainComponent } from './main.component';
+import { CareerCardComponent } from './components/career-card/career-card.component';
+import { IconComponent } from './components/icon/icon.component'
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    MainRoutingModule
-  ]
+  declarations: [MainComponent, CareerCardComponent, IconComponent],
+  imports: [CommonModule, MainRoutingModule],
 })
-export class MainModule { }
+export class MainModule {}
