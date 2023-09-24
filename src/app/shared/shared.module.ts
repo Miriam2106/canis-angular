b68e9a898101fcc489ed5d45cf7937dc4945fd3e
Ricardo C. <20203tn077@utez.edu.mx>
2023-09-23 23:48:06 -0600

    chore(global): add font awesome

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,19 @@ import { CommonModule } from '@angular/common'
 import { NavbarComponent } from './components/navbar/navbar.component'
 import { FooterComponent } from './components/footer/footer.component'
 import { RelativePipe } from './pipes/relative.pipe'
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 
 @NgModule({
   declarations: [NavbarComponent, FooterComponent, RelativePipe],
   imports: [CommonModule],
-  exports: [NavbarComponent, FooterComponent, RelativePipe],
+  exports: [NavbarComponent, FooterComponent, RelativePipe, FontAwesomeModule],
 })
-export class SharedModule {}
+export class SharedModule {
+  constructor(iconLibrary: FaIconLibrary) {
+    iconLibrary.addIconPacks(fas)
+  }
+}
